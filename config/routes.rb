fe155271777bfa249f8929ce26c6d595e18a1d4d
pickledyamsman <pickledyamsman@gmail.com>
2016-08-01 18:31:47 -0400

    finished chapter 14 I1 and I2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'login' => :destroy
+  end
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
